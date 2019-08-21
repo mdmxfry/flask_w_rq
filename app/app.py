de4093c3bbf03c1b0fe6d2313c275684680a8121
Max Fry <m.sukach@innopolis.ru>
2019-08-21 11:24:30 +0300

    Initial comm

---
+++ app/app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+
+import os
+import time
+
+from flask import Flask
+from celery import Celery
+
+app = Flask(__name__)
+app.config['CELERY_BROKER_URL'] = 'redis://redis:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://redis:6379/0'
+
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
+
+
+@app.route('/')
+def hello_world():
+    task = load.delay('test')
+    return 'Started task'
+
+
+@celery.task
+def load(arg1):
+    time.sleep(10)
+    return arg1
+
+
+if __name__ == '__main__':
+    app.run()
