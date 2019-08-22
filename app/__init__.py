997f1d9cbf90068a30dffdb657c7616c948cb77f
Max Fry <m.sukach@innopolis.ru>
2019-08-22 09:39:40 +0300

    Added nginx and gunicorn

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,27 @@
-#!/usr/bin/python3.6
+#!/usr/bin/python3.7
 # -*- coding: utf-8 -*-
 
+import os
+import time
 
-if __name__ == '__main__':
-    pass
+from flask import Flask
+from celery import Celery
+
+app = Flask(__name__)
+app.config['CELERY_BROKER_URL'] = 'redis://redis:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://redis:6379/0'
+
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
+
+
+@app.route('/')
+def hello_world():
+    task = load.delay('test')
+    return 'Started task'
+
+
+@celery.task
+def load(arg1):
+    time.sleep(10)
+    return arg1
---
+++ wsgi.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3.6
+# -*- coding: utf-8 -*-
+
+from app import app
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
